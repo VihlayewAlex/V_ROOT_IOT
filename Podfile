8325b6380b1b608c1fd4973de27529a038eb21f9
VihlayewAlex <vihlayew.alex@gmail.com>
2018-02-17 10:48:33 +0200

    Integrated Cocoapods, add project dependencies

---
+++ Podfile
@@ -0,0 +1,13 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'V_ROOT_IOT' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for V_ROOT_IOT
+  pod "PromiseKit"
+  pod "Alamofire"
+  pod "PromiseKit/Alamofire"
+
+end
---
+++ V_ROOT_IOT/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  V_ROOT_IOT
+//
+//  Created by Alex Vihlayew on 2/17/18.
+//  Copyright © 2018 Alex Vihlayew. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
